4e64110bf62098d4b3dac7550e79782b31f04626
Tim Weisenberger <tim.weisenberger@netlogix.de>
2022-01-07 09:47:59 +0100

    FEATURE: Add TYPO3 v10 support Original: f5e1b5c3cefb34279e07ebc7f80c9e43afc41210

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.1.99',
+            'typo3' => '8.7.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
