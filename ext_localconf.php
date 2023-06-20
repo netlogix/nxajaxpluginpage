2cd60d314e75e1780f98bcbd505eb5f78a0ac5aa
Sascha Nowak <sascha.nowak@netlogix.de>
2023-06-20 07:41:01 +0200

    FEATURE: Add TYPO3 12.4 support

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,8 +2,6 @@
 defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
-
-	$class = \Netlogix\Nxajaxpluginpage\Service\AcceptContentToHashBaseService::class;
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['createHashBase'][$class] = $class . '->createHashBase';
-
+    $class = \Netlogix\Nxajaxpluginpage\Service\AcceptContentToHashBaseService::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['createHashBase'][$class] = $class . '->createHashBase';
 });
